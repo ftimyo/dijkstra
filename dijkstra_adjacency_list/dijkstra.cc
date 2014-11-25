f3348d65a56038e7a821563d81016413642d8322
Tim <tim@Narwhal.local>
2014-11-24 22:30:20 -0500

    handle reachable vertices

--- dijkstra_adjacency_list/dijkstra.cc
+++ dijkstra_adjacency_list/dijkstra.cc
@@ -1,160 +1,182 @@
-#include <map>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <string>
 #include <fstream>
 #include <stdio.h>
+#include <utility>
+#include <tuple>
+#include <limits>
+
+namespace dijkstra_algo {
+
 using std::string;
 using std::vector;
-using std::map;
-static const int INFINITE = 1 << 10;
-
-struct vertex {
-	struct out_edge {
-		vertex	*v;
-		int weight;
-		out_edge(vertex *vp, int w):v{vp}, weight{w}{}
-	};
-
-	vertex(const string& name):id{name}, adj{}, dist{INFINITE}, path{NULL}{}
-	virtual ~vertex() {}
-	string id;
-	vector<out_edge> adj;
-	int dist;
-	vertex *path;
-};
-
 using std::push_heap;
 using std::pop_heap;
 using std::make_heap;
+using std::pair;
+using std::tuple;
+using std::get;
+using std::set;
 
-struct graph {
-	struct edge {
-		string v_start;
-		string v_end;
-		int weight;
-		edge(const string& s, const string& e, int w): v_start{s},
-		  v_end{e}, weight{w}{}
+class Graph {
+ public:
+	static const int kInfinite = std::numeric_limits<int>::max();
+
+	typedef tuple<string, string, int> edge_type;
+
+	struct Vertex {
+		Vertex(const string& vertex_id) : id{vertex_id}, adj{},
+				dist{Graph::kInfinite}, path{NULL} {}
+		virtual ~Vertex(){}
+
+		string	id;
+		int			dist;
+		Vertex	*path;
+		vector<pair<Vertex*, int>> adj;
 	};
-	map<string,vertex*> origin;
-	vector<vertex*>	paths;
-	graph():origin{}, paths{}{}
-	virtual ~graph() {
-		for_each(origin.begin(), origin.end(), [](map<string,vertex*>::value_type& p) {
-			delete p.second;
-		});
-	}
-	
-	struct vertex_comp {
-		bool operator()(const vertex *v1, const vertex *v2) {
-			return v1->dist > v2->dist;
+
+	struct VertexComp {
+		bool operator() (Vertex *v1, Vertex *v2) {
+		 return v1->dist > v2->dist;
 		}
 	};
-	void get_paths(const string& source);
-	void print_all_paths(FILE *os);
-	void print_path(FILE *os, vertex *v);
-	void print_vertices(FILE *os);
-	void load_graph(const vector<edge>& E);
-};
 
-void graph::print_path(FILE *os, vertex *v)
-{
+	struct VertexIdComp {
+		bool operator() (Vertex *v1, Vertex *v2) {
+			return v1->id < v2->id;
+		}
+	};
+
+	typedef set<Vertex*, VertexIdComp> VertexSet;
+
+ public:
+	Graph() : edges_{}, paths_{} {}
+	virtual ~Graph() {
+		for (auto& v : paths_) {
+			delete v;
+		}
+	}
+
+	void GetShortestPaths(const string& id);
+	void PrintAllPaths(FILE *os);
+	void AddEdge(const string& start, const string& end, int weight);
+
+ private:
+	void GetShortestPaths(Vertex *source, VertexSet& v_set);
+	void PrintPath(FILE *os, Vertex *v);
+	Vertex* UpdateSet(VertexSet& set, const string& id);
+	void GetAdjacencyList(VertexSet& adj);
+	vector<edge_type> edges_;
+	vector<Vertex*> paths_;
+}; 
+
+void Graph::GetShortestPaths(const string& id) {
+	VertexSet v_set;
+	GetAdjacencyList(v_set);
+	Vertex v{id};
+	auto pos = v_set.find(&v);
+	if (pos == v_set.end()) {
+		return;
+	} else {
+		GetShortestPaths(*pos, v_set);
+	}
+}
+
+void Graph::GetShortestPaths(Graph::Vertex *source, Graph::VertexSet& v_set) {
+	vector<Vertex*> heap;
+	source->dist = 0;
+	heap.push_back(source);
+	push_heap(heap.begin(), heap.end(), VertexComp{});
+
+	while (!heap.empty()) {
+		auto s = heap.front();
+		pop_heap(heap.begin(), heap.end(), VertexComp{});
+		v_set.erase(s);
+		heap.pop_back();
+		paths_.push_back(s);
+
+		for (auto v : s->adj) {
+			int dist = s->dist + v.second;
+
+			if (v.first->dist == Graph::kInfinite) {
+				v_set.erase(v.first);
+				v.first->dist = dist;
+				v.first->path = s;
+				heap.push_back(v.first);
+				push_heap(heap.begin(), heap.end(), VertexComp{});
+			} else if (dist < v.first->dist) {
+				v.first->dist = dist;
+				v.first->path = s;
+				make_heap(heap.begin(), heap.end(), VertexComp{});
+			} 
+		}
+	}
+	
+	for (auto v : v_set) {
+		paths_.push_back(v);
+	}
+}
+
+
+Graph::Vertex* Graph::UpdateSet(VertexSet& v_set, const string& id) {
+	Vertex v{id};
+	auto pos = v_set.equal_range(&v);
+	if (pos.first == pos.second) {
+		auto v = new Vertex{id};
+		v_set.emplace_hint(pos.first, v);
+		return v;
+	} 
+	return *(pos.first);
+}
+
+void Graph::GetAdjacencyList(VertexSet& v_set) {
+	for (auto& edge : edges_) {
+		Graph::Vertex *s = UpdateSet(v_set, get<0>(edge));
+		Graph::Vertex *e = UpdateSet(v_set, get<1>(edge));
+		s->adj.emplace_back(e, get<2>(edge));
+	}
+}
+
+void Graph::PrintPath(FILE *os, Graph::Vertex *v) {
 	if (v->path != NULL) {
-		print_path(os, v->path);
+		PrintPath(os, v->path);
 		fprintf(os, " -> %s", v->id.c_str());
 	} else {
 		fprintf(os, "%s", v->id.c_str());
 	}
 }
 
-void graph::print_all_paths(FILE *os)
-{
-	for(auto& v : paths) {
-		fprintf(os, "%s (distance %d) path:\t", v->id.c_str(), v->dist);
-		print_path(os, v);
-		fprintf(os, "\n");
-	}
-}
-
-void graph::get_paths(const string& source)
-{
-	vertex *s = origin[source];
-	vector<vertex*> heap;
-	if (s == NULL) {
-		fprintf(stderr, "source %s is not in the graph\n", source.c_str());
-	}
-
-	s->dist = 0;
-	s->path = NULL;
-	heap.push_back(s);
-	push_heap(heap.begin(), heap.end(), vertex_comp{});
-
-	while (!heap.empty()) {
-		vertex *v = heap.front();
-		paths.push_back(v);
-		pop_heap(heap.begin(), heap.end(), vertex_comp{});
-		heap.pop_back();
-		for (auto& e : v->adj) {
-			int dist = v->dist + e.weight;
-			if (e.v->dist == INFINITE) {
-				e.v->dist = dist;
-				e.v->path = v;
-				heap.push_back(e.v);
-				push_heap(heap.begin(), heap.end(), vertex_comp{});
-			} else if (dist < e.v->dist) {
-				e.v->dist = dist;
-				e.v->path = v;
-				make_heap(heap.begin(), heap.end(), vertex_comp{});
-			}
+void Graph::PrintAllPaths(FILE *os) {
+	fprintf(os, "Source vertex: %s\n", paths_[0]->id.c_str());
+	for(auto& v : paths_) {
+		if (v->dist == Graph::kInfinite) {
+			fprintf(os, "%s (distance Inifinte) No path\n", v->id.c_str());
+		} else {
+			fprintf(os, "%s (distance %d) path:\t", v->id.c_str(), v->dist);
+			PrintPath(os, v);
+			fprintf(os, "\n");
 		}
 	}
 }
 
-void graph::load_graph(const vector<graph::edge>& E)
-{
-	for (auto& e : E) {
-		auto& start = origin[e.v_start];
-		auto& end = origin[e.v_end];
-		if (start == NULL) {
-			start = new vertex{e.v_start};
-		}
-		if (end == NULL) {
-			end = new vertex{e.v_end};
-		}
-		start->adj.push_back(vertex::out_edge{end, e.weight});
-	}
+void Graph::AddEdge(const string& start, const string& end, int weight) {
+	edges_.emplace_back(start, end, weight);
 }
 
-void graph::print_vertices(FILE *os)
-{
-	fprintf(os, "origin V set (%lu vertice)\n", origin.size());
-	for (auto& v : origin) {
-		fprintf(os, "%s:\t", v.second->id.c_str());
-		for (auto& e : v.second->adj) {
-			fprintf(os, " %s", e.v->id.c_str());
-		}
-		fprintf(os, "\n");
-	}
-}
-
-void load_graph_from_file(const char* filename, std::vector<graph::edge>& E)
-{
-	std::ifstream is{filename};
-	string s, e;
-	int w;
-	while(is>>s>>e>>w) {
-		E.emplace_back(s, e, w);
-	}
-}
+}//namespace dijkstra
 
 int main()
 {
-	vector<graph::edge> E;
-	load_graph_from_file("digraph", E);
-	graph djikstra;
-	djikstra.load_graph(E);
-//	djikstra.get_paths(E[0].v_start.c_str());
-	djikstra.get_paths("v1");
-	djikstra.print_all_paths(stdout);
+	std::ifstream is{"digraph"};
+	std::string s, e;
+	int w;
+	dijkstra_algo::Graph dijkstra;
+	while(is>>s>>e>>w) {
+		dijkstra.AddEdge(s, e, w);
+	}
+	dijkstra.GetShortestPaths("v1");
+	dijkstra.PrintAllPaths(stdout);
 	return 0;
 }
